4e32dbcdcdcd3fdf2d1d2d5376e188e8726f295a
Gabriel773-bit <mastergameplus2@gmail.com>
2025-03-31 08:51:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+ //Criar o vetor ARRAY
+
+ var nomes = ["Elias", "Gabriel", "Erick", "João", "Arthur", "Clara", "Ana", "Gabriel Salomão", 
+ "Felipe", "Gabriela", "Gustavo", "Igor", "Bruno", "João Pedro", "Filipe"]
+
+ var nomeEscolhido = parseInt(prompt("Digite um numero de 0 a 14"))
+ //Concaternar
+ document.write("Nome escolhido: " + nomes[nomeEscolhido])
