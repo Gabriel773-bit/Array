64eb7a5e3162cb71f7f8329e23f0b80d88ff40d3
Gabriel773-bit <mastergameplus2@gmail.com>
2025-04-14 08:22:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
- //Criar o vetor ARRAY
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula Array</title>
+</head>
+<body>
+    <h1>Aula Array</h1>
+    
+    <script src="script.js">
+         //Criar o vetor ARRAY
 
  var nomes = ["Elias", "Gabriel", "Erick", "João", "Arthur", "Clara", "Ana", "Gabriel Salomão", 
- "Felipe", "Gabriela", "Gustavo", "Igor", "Bruno", "João Pedro", "Filipe"]
+ "Felipe", "Gabriela", "Gustavo", "Igor", "Bruno", "João Pedro", "Filipe", "John", "Bruno", "Sandro", "Curió",
+ "Vasco", "Ana Paula","Flavia","Flavio","Caue","Paulo", "Adirson","Edson","Arthur Santos", "Saulo Bom Homem", "Walter White", "Tom Holand"]
 
- var nomeEscolhido = parseInt(prompt("Digite um numero de 0 a 14"))
+ var nomeEscolhido = parseInt(prompt("O numero de indentificação da pessoa"))
  //Concaternar
- document.write("Nome escolhido: " + nomes[nomeEscolhido])
+ document.write("Individo: " + nomes[nomeEscolhido])
+    </script>
+</body>
+</html>
